30758cc41713c149c5e62aeb7692954e9af66fbc
AlexHGama <aalexhgama@bol.com.br>
2022-12-02 10:39:24 -0300

    Updates

---
+++ precache-manifest.366adcf973e795d90b7ea44906eb6e0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66026aa9c82df44b5790",
+    "url": "/pagamento/static/css/main.4f14dcb3.chunk.css"
+  },
+  {
+    "revision": "66026aa9c82df44b5790",
+    "url": "/pagamento/static/js/main.66026aa9.chunk.js"
+  },
+  {
+    "revision": "a6a1f2f3f0a3c8679a77",
+    "url": "/pagamento/static/js/1.a6a1f2f3.chunk.js"
+  },
+  {
+    "revision": "5db5038b806a5939586e",
+    "url": "/pagamento/static/js/runtime~main.5db5038b.js"
+  },
+  {
+    "revision": "1a8c7aece465910ae90e23cf6aa69010",
+    "url": "/pagamento/index.html"
+  }
+];
